d755fc96713dc0a27c6fdd88bbfc463885dc8f9c
Kristijan Armeni <kristijan.armeni@gmail.com>
2025-06-12 08:41:46 -0400

    make sure raw data is only scanned and loaded once

--- app.py
+++ app.py
@@ -21,13 +21,9 @@ question_circle_fill = ui.HTML(
     '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-circle-fill mb-1" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247zm2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z"/></svg>'
 )
 
+# this is run only once
+def load_raw_data():
 
-@lru_cache(maxsize=32)
-def load_raw_data_subset(time_start, time_end, user_id, hashtag):
-    # Convert datetime objects to strings for caching
-    time_start_str = time_start.isoformat() if hasattr(time_start, 'isoformat') else str(time_start)
-    time_end_str = time_end.isoformat() if hasattr(time_end, 'isoformat') else str(time_end)
-    
     lf = pl.scan_parquet(source=DATA_RAW)
 
     df_ = (
@@ -41,13 +37,30 @@ def load_raw_data_subset(time_start, time_end, user_id, hashtag):
         .select(pl.col(COL_AUTHOR_ID), pl.col(COL_TIME), pl.col(COL_POST))
         .with_columns(pl.col(COL_TIME).str.to_datetime("%m/%d/%Y %H:%M"))
         .with_columns(pl.col(COL_TIME).dt.replace_time_zone("UTC"))
-        .filter(
+        .sort(by=COL_TIME)
+    ).collect()
+
+    return df_
+
+
+@lru_cache(maxsize=32)
+def get_raw_data_subset(time_start, time_end, user_id, hashtag):
+    # Convert datetime objects to strings for caching
+    time_start_str = time_start.isoformat() if hasattr(time_start, 'isoformat') else str(time_start)
+    time_end_str = time_end.isoformat() if hasattr(time_end, 'isoformat') else str(time_end)
+    
+    # make sure the raw data are loaded only once
+    global df_raw
+    if df_raw is None:
+        df_raw = load_raw_data()
+    
+    df_ = (
+        df_raw.filter(
             pl.col(COL_AUTHOR_ID) == user_id,
             pl.col(COL_TIME).is_between(lower_bound=time_start, upper_bound=time_end),
             pl.col(COL_POST).str.contains(hashtag),
         )
-        .sort(by=COL_TIME)
-    ).collect()
+    )
 
     return df_
 
@@ -59,8 +72,9 @@ def load_primary_output():
     return df
 
 
-# Lazy load primary data
+# Set global variables
 df = None
+df_raw = None 
 
 # Time step will be calculated when data is loaded
 time_step = None
@@ -82,8 +96,6 @@ def select_users(secondary_output, selected_hashtag):
     )
 
     return users_df
-
-
 
 
 page_dependencies = ui.tags.head(
@@ -360,7 +372,7 @@ def server(input, output, session):
     def tweets():
         timewindow = get_selected_datetime()
 
-        df_posts = load_raw_data_subset(
+        df_posts = get_raw_data_subset(
             time_start=timewindow,
             time_end=timewindow + get_time_step(),
             user_id=input.user_picker(),
